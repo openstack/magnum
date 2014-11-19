eb16491c6deca6a8efa82b28e84a07a66527c2ca
digambarpatil15 <digambarpatil15@yahoo.co.in>
2014-11-19 22:33:14 +0530

    Replaces Solum with Magnum. Added doc string for the Magnum API.
    Implements: blueprint magnum-api-service
    Change-Id: Iefec7a34ca4f9533f95a0f6c5f0784b86f93af58

--- magnum/config.py
+++ magnum/config.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-"""Solum specific config handling."""
+"""Magnum specific config handling."""
 
 from oslo.config import cfg
 
