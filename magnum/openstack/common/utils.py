e36e8ee5d0201b1f47158028e14225537c353d18
Kennan <wkqwu@cn.ibm.com>
2014-12-31 17:06:30 +0800

    change old oslo.concurrency to oslo_concurrency
        as oslo.concurrency deprecated, need to sync to latest usage
    
    Change-Id: I151f2ef3d786745ef11f9b9c2be2aff167218fff

--- magnum/openstack/common/utils.py
+++ magnum/openstack/common/utils.py
@@ -9,7 +9,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-from oslo.concurrency import processutils
+from oslo_concurrency import processutils
 
 
 def execute(*cmd, **kwargs):
