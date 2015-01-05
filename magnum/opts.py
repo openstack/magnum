7daa7b434d1632b391f3974f7eb434ba78f59202
Kennan <wkqwu@cn.ibm.com>
2015-01-05 18:17:17 +0800

    Fix the opts in genconfig issue
    there is a lot of options not include in opts, which cause genconfig
    not include such config, this patch fix that
    
    Change-Id: I3774930563644b402b74b66b776f13d810fe6f32

--- magnum/opts.py
+++ magnum/opts.py
@@ -17,14 +17,36 @@ import itertools
 
 import magnum.api.app
 import magnum.api.auth
-import magnum.backend.config
+import magnum.common.clients
+import magnum.common.exception
+import magnum.common.magnum_keystoneclient
 import magnum.conductor.config
+import magnum.conductor.handlers.bay_k8s_heat
+import magnum.conductor.handlers.docker_conductor
+import magnum.db.sqlalchemy.models
+import magnum.openstack.common.eventlet_backdoor
+import magnum.openstack.common.log
+import magnum.openstack.common.periodic_task
 
 
 def list_opts():
     return [
         ('DEFAULT',
          itertools.chain(magnum.api.app.API_SERVICE_OPTS,
-                         magnum.api.auth.AUTH_OPTS,)),
+                         magnum.api.auth.AUTH_OPTS,
+                         magnum.common.exception.exc_log_opts,
+                         magnum.common.magnum_keystoneclient.trust_opts,
+                         magnum.common.paths.PATH_OPTS,
+                         magnum.common.utils.UTILS_OPTS,
+                         (magnum.openstack.common.eventlet_backdoor
+                          .eventlet_backdoor_opts),
+                         magnum.openstack.common.log.generic_log_opts,
+                         magnum.openstack.common.log.log_opts,
+                         magnum.openstack.common.periodic_task.periodic_opts,
+                         )),
         ('conductor', magnum.conductor.config.SERVICE_OPTS),
+        ('database', magnum.db.sqlalchemy.models.sql_opts),
+        ('docker', magnum.conductor.handlers.docker_conductor.docker_opts),
+        ('heat_client', magnum.common.clients.heat_client_opts),
+        ('k8s_heat', magnum.conductor.handlers.bay_k8s_heat.k8s_heat_opts),
     ]
