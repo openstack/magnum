46ebff53e6c5042b3ecbcf97830e81b1c10ec6a5
Andrew Melton <andrew.melton@rackspace.com>
2015-01-06 13:20:59 -0800

    Docker: Pull Image Before Container Create
    The image needs to be pulled so that it's available for
    subsequent inspect and create calls.
    
    Change-Id: I7f2731911a3fc71fbbf3b408b39e57b7521db81f
    Closes-bug: #1408101

---
+++ magnum/common/docker_utils.py
@@ -0,0 +1,25 @@
+# Copyright 2015 Rackspace  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+def parse_docker_image(image_id):
+    image_parts = image_id.split(':', 1)
+
+    image_repo = image_parts[0]
+    image_tag = None
+
+    if len(image_parts) > 1:
+        image_tag = image_parts[1]
+
+    return image_repo, image_tag
