5e02fc0e145c8604906bb1200f30fc4ca690f95d
Andrew Melton <andrew.melton@rackspace.com>
2015-04-22 16:02:16 -0400

    Use container's bay for docker endpoint
    Instead of using a config value to create the Docker conductor's
    Docker client, use the api_address from the bay the Container
    is created in.
    
    Change-Id: I28b136dc6f9b9970c1f418d550efa5326961bce1
    Implements: bp docker-conductor-connect-to-docker-bay
    Closes-Bug: #1405356
    Closes-Bug: #1442556

--- magnum/conductor/handlers/docker_conductor.py
+++ magnum/conductor/handlers/docker_conductor.py
@@ -17,20 +17,15 @@ from oslo_config import cfg
 
 from magnum.common import docker_utils
 from magnum.common import exception
+from magnum.common import utils
 from magnum.conductor.handlers.common import docker_client
+from magnum import objects
 from magnum.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
 CONF = cfg.CONF
 
 docker_opts = [
-    cfg.StrOpt('root_directory',
-               default='/var/lib/docker',
-               help='Path to use as the root of the Docker runtime.'),
-    cfg.StrOpt('host_url',
-               default='unix:///var/run/docker.sock',
-               help='tcp://host:port to bind/connect to or '
-                    'unix://path/to/socket to use'),
     cfg.StrOpt('docker_remote_api_version',
                default=docker_client.DEFAULT_DOCKER_REMOTE_API_VERSION,
                help='Docker remote api version. Override it according to '
@@ -56,18 +51,11 @@ class Handler(object):
 
     def __init__(self):
         super(Handler, self).__init__()
-        self._docker = None
 
-    @property
-    def docker(self):
-        if self._docker is None:
-            self._docker = docker_client.DockerHTTPClient(CONF.docker.host_url,
-                CONF.docker.docker_remote_api_version)
-        return self._docker
-
-    def _find_container_by_name(self, name):
+    @staticmethod
+    def _find_container_by_name(docker, name):
         try:
-            for info in self.docker.list_instances(inspect=True):
+            for info in docker.list_instances(inspect=True):
                 if info['Config'].get('Hostname') == name:
                     return info
         except errors.APIError as e:
@@ -78,104 +66,121 @@ class Handler(object):
     def _encode_utf8(self, value):
         return unicode(value).encode('utf-8')
 
+    @staticmethod
+    def _docker_for_bay(bay):
+        tcp_url = 'tcp://%s:2376' % bay.api_address
+        return docker_client.DockerHTTPClient(tcp_url,
+                                        CONF.docker.docker_remote_api_version)
+
+    @classmethod
+    def _docker_for_container(cls, context, container):
+        bay = objects.Bay.get_by_uuid(context, container.bay_uuid)
+        return cls._docker_for_bay(bay)
+
+    @classmethod
+    def get_docker_client(cls, context, container):
+        if utils.is_uuid_like(container):
+            container = objects.Container.get_by_uuid(context, container)
+        return cls._docker_for_container(context, container)
+
     # Container operations
 
     def container_create(self, context, name, container_uuid, container):
+        docker = self.get_docker_client(context, container)
         image_id = container.image_id
         LOG.debug('Creating container with image %s name %s'
                   % (image_id, name))
         try:
             image_repo, image_tag = docker_utils.parse_docker_image(image_id)
-            self.docker.pull(image_repo, tag=image_tag)
-            self.docker.inspect_image(self._encode_utf8(container.image_id))
-            self.docker.create_container(image_id, name=name,
-                                         hostname=container_uuid,
-                                         command=container.command)
+            docker.pull(image_repo, tag=image_tag)
+            docker.inspect_image(self._encode_utf8(container.image_id))
+            docker.create_container(image_id, name=name,
+                                    hostname=container_uuid,
+                                    command=container.command)
             return container
-        except errors.APIError as api_error:
-            raise exception.ContainerException(
-                      "Docker API Error : %s" % str(api_error))
-
-    def container_list(self, context):
-        LOG.debug("container_list")
-        try:
-            container_list = self.docker.containers()
-            return container_list
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_delete(self, context, container_uuid):
         LOG.debug("container_delete %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
+            docker_id = self._find_container_by_name(docker, container_uuid)
             if not docker_id:
                 return None
-            return self.docker.remove_container(docker_id)
+            return docker.remove_container(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_show(self, context, container_uuid):
         LOG.debug("container_show %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return self.docker.inspect_container(docker_id)
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return docker.inspect_container(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_reboot(self, context, container_uuid):
         LOG.debug("container_reboot %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return self.docker.restart(docker_id)
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return docker.restart(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_stop(self, context, container_uuid):
         LOG.debug("container_stop %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return self.docker.stop(docker_id)
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return docker.stop(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_start(self, context, container_uuid):
         LOG.debug("Starting container %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
+            docker_id = self._find_container_by_name(docker, container_uuid)
             LOG.debug("Found Docker container %s" % docker_id)
-            return self.docker.start(docker_id)
+            return docker.start(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_pause(self, context, container_uuid):
         LOG.debug("container_pause %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return self.docker.pause(docker_id)
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return docker.pause(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_unpause(self, context, container_uuid):
         LOG.debug("container_unpause %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return self.docker.unpause(docker_id)
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return docker.unpause(docker_id)
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
 
     def container_logs(self, context, container_uuid):
         LOG.debug("container_logs %s" % container_uuid)
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return {'output': self.docker.get_container_logs(docker_id)}
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return {'output': docker.get_container_logs(docker_id)}
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
@@ -183,9 +188,10 @@ class Handler(object):
     def container_execute(self, context, container_uuid, command):
         LOG.debug("container_execute %s command %s" %
                   (container_uuid, command))
+        docker = self.get_docker_client(context, container_uuid)
         try:
-            docker_id = self._find_container_by_name(container_uuid)
-            return {'output': self.docker.execute(docker_id, command)}
+            docker_id = self._find_container_by_name(docker, container_uuid)
+            return {'output': docker.execute(docker_id, command)}
         except errors.APIError as api_error:
             raise exception.ContainerException(
                       "Docker API Error : %s" % str(api_error))
