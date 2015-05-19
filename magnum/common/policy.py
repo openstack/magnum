c317bea2091c5c129a10a6f9d318c57c3210aa56
yuntongjin <yuntongjin@gmail.com>
2015-05-20 07:39:40 +0800

    introduce policy for magnum
    This patch introduce oslo.policy for magnum and also add policy
    rules in a policy.json file.
    
    we can define policy rules refer to
      http://docs.openstack.org/developer/oslo.policy/api.html
    
    Co-Authored-By: ShaoHe Feng <shaohe.feng@intel.com>
    Change-Id: I37f8b0ba8108162e1f826780ff43c8d21f476209
    Partial-implements: blueprint policy-enfore

---
+++ magnum/common/policy.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2015 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Policy Engine For magnum."""
+
+from oslo_config import cfg
+from oslo_policy import policy
+
+
+_ENFORCER = None
+CONF = cfg.CONF
+
+
+# we can get a policy enforcer by this init.
+# oslo policy support change policy rule dynamically.
+# at present, policy.enforce will reload the policy rules when it checks
+# the policy files have been touched.
+def init(policy_file=None, rules=None,
+         default_rule=None, use_conf=True, overwrite=True):
+    """Init an Enforcer class.
+
+        :param policy_file: Custom policy file to use, if none is
+                            specified, ``conf.policy_file`` will be
+                            used.
+        :param rules: Default dictionary / Rules to use. It will be
+                      considered just in the first instantiation. If
+                      :meth:`load_rules` with ``force_reload=True``,
+                      :meth:`clear` or :meth:`set_rules` with
+                      ``overwrite=True`` is called this will be overwritten.
+        :param default_rule: Default rule to use, conf.default_rule will
+                             be used if none is specified.
+        :param use_conf: Whether to load rules from cache or config file.
+        :param overwrite: Whether to overwrite existing rules when reload rules
+                          from config file.
+    """
+    global _ENFORCER
+    if not _ENFORCER:
+        # http://docs.openstack.org/developer/oslo.policy/usage.html
+        _ENFORCER = policy.Enforcer(CONF,
+                                    policy_file=policy_file,
+                                    rules=rules,
+                                    default_rule=default_rule,
+                                    use_conf=use_conf,
+                                    overwrite=overwrite)
+    return _ENFORCER
+
+
+def enforce(context, action=None, target=None,
+            do_raise=True, exc=None, *args, **kwargs):
+
+    """Checks authorization of a rule against the target and credentials.
+
+        :param dict context: As much information about the user performing the
+                             action as possible.
+        :param action: The rule to evaluate.
+        :param dict target: As much information about the object being operated
+                            on as possible.
+        :param do_raise: Whether to raise an exception or not if check
+                         fails.
+        :param exc: Class of the exception to raise if the check fails.
+                    Any remaining arguments passed to :meth:`enforce` (both
+                    positional and keyword arguments) will be passed to
+                    the exception class. If not specified,
+                    :class:`PolicyNotAuthorized` will be used.
+
+        :return: ``False`` if the policy does not allow the action and `exc` is
+                 not provided; otherwise, returns a value that evaluates to
+                 ``True``.  Note: for rules using the "case" expression, this
+                 ``True`` value will be the specified string from the
+                 expression.
+    """
+    enforcer = init()
+    credentials = context.to_dict()
+    if target is None:
+        target = {'project_id': context.project_id,
+                  'user_id': context.user_id}
+    return enforcer.enforce(action, target, credentials,
+                            do_raise=do_raise, exc=exc, *args, **kwargs)
