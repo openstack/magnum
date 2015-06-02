fb9d067035bd9f3e66e3d3ac29755ace625df12e
Kai Qiang Wu(Kennan) <wkq5325@gmail.com>
2015-06-02 09:55:56 +0800

    Remove old hack for requirements
    As magnum has been added in requirements project, we
    not need to add such hack any more.
    
    Closes-Bug: #1460874
    Change-Id: I4f661c398186067cd2b5a1f3a13ae77bb3809396

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -12,10 +12,6 @@ if is_service_enabled m-api m-cond; then
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         echo_summary "Before Installing magnum"
         mkdir -p $SCREEN_LOGDIR
-        if [[ -z `grep 'docker-py' ${REQUIREMENTS_DIR}/global-requirements.txt` ]]; then
-            sed -i 's/requests>=.*/requests>=2.5.2/g' ${REQUIREMENTS_DIR}/global-requirements.txt
-            echo "docker-py>=1.1.0" >> ${REQUIREMENTS_DIR}/global-requirements.txt
-        fi
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         echo_summary "Installing magnum"
         install_magnum
