c41e2f97a82f138ce646774b9181379345def2c2
Zane Bitter <zbitter@redhat.com>
2015-06-03 12:34:05 -0400

    Add license header to swagger.py
    We don't want anyone to think we're responsible for this awful, awful
    code :)
    
    Change-Id: I8d636098971d6bb89b835b866285d84a4c8d03fb
    Closes-Bug: #1461607

--- magnum/common/pythonk8sclient/client/swagger.py
+++ magnum/common/pythonk8sclient/client/swagger.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+
+# Copyright 2015 SmartBear Software
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 """Swagger generic API client. This client handles the client-
 server communication, and is invariant across implementations. Specifics of
 the methods and models for each application are generated from the Swagger
