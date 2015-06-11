617b772e556a9cf874031a9068a9e31156242aa0
Dane LeBlanc <leblancd@cisco.com>
2015-06-11 16:01:22 -0400

    Modify magnum api context to use user_name and project_name
    Problem description:
    If DevStack is used to instantiate the magnum plugin, and the
    devstack localrc/local.conf has the default values for:
       LOG_COLOR (default value = True)
       SYSLOG (default value = False)
    then upon startup (i.e. running DevStack's stack.sh), the magnum devstack
    lib calls the DevStack common setup_colorized_logging function, but
    without passing the optional 'project_var' and 'user_var' arguments to
    this function. As a result, the setup_colorized_logging
    function uses its default values of "user_name" and "project_name"
    when it defines the logging_context_format_string (which in turn gets
    configured in /etc/magnum/magnum.conf). The problem is that "user_name"
    and "project_name" are not defined in the API context used by Magnum,
    so that whenever the magnum plugin does a logging call, a KeyError
    exception for the non-existant key "user_name" is generated.
    
    Fix description:
    The fix is to modify the Magnum context to use "user_name" and
    "project_name" attributes to be consistent with the default context
    format string set up by DevStack.
    
    Change-Id: Ia0c34899609735ff9d8b4597101e004e2684657e
    Closes-Bug: #1464376

--- magnum/common/context.py
+++ magnum/common/context.py
@@ -17,10 +17,11 @@ class RequestContext(context.RequestContext):
     """Extends security contexts from the OpenStack common library."""
 
     def __init__(self, auth_token=None, auth_url=None, domain_id=None,
-                 domain_name=None, user=None, user_id=None, project=None,
-                 project_id=None, is_admin=False, is_public_api=False,
-                 read_only=False, show_deleted=False, request_id=None,
-                 trust_id=None, auth_token_info=None, **kwargs):
+                 domain_name=None, user_name=None, user_id=None,
+                 project_name=None, project_id=None, is_admin=False,
+                 is_public_api=False, read_only=False, show_deleted=False,
+                 request_id=None, trust_id=None, auth_token_info=None,
+                 **kwargs):
         """Stores several additional request parameters:
 
         :param domain_id: The ID of the domain.
@@ -30,8 +31,9 @@ class RequestContext(context.RequestContext):
 
         """
         self.is_public_api = is_public_api
+        self.user_name = user_name
         self.user_id = user_id
-        self.project = project
+        self.project_name = project_name
         self.project_id = project_id
         self.domain_id = domain_id
         self.domain_name = domain_name
@@ -40,7 +42,8 @@ class RequestContext(context.RequestContext):
         self.trust_id = trust_id
 
         super(RequestContext, self).__init__(auth_token=auth_token,
-                                             user=user, tenant=project,
+                                             user=user_name,
+                                             tenant=project_name,
                                              is_admin=is_admin,
                                              read_only=read_only,
                                              show_deleted=show_deleted,
@@ -52,9 +55,9 @@ class RequestContext(context.RequestContext):
                       'auth_url': self.auth_url,
                       'domain_id': self.domain_id,
                       'domain_name': self.domain_name,
-                      'user': self.user,
+                      'user_name': self.user_name,
                       'user_id': self.user_id,
-                      'project': self.project,
+                      'project_name': self.project_name,
                       'project_id': self.project_id,
                       'is_admin': self.is_admin,
                       'is_public_api': self.is_public_api,
