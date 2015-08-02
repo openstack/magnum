73de30eba294811c89ce506858550f4f3f09d05a
Hua Wang <wanghua.humble@gmail.com>
2015-08-03 05:29:22 +0800

    Remove coding:utf-8
    Change-Id: I57f1e5febf678297db492acccf34100276b89483

--- magnum/api/auth.py
+++ magnum/api/auth.py
@@ -1,6 +1,4 @@
-# -*- encoding: utf-8 -*-
-#
-# Copyright © 2012 New Dream Network, LLC (DreamHost)
+# Copyright 2012 New Dream Network, LLC (DreamHost)
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
--- magnum/api/config.py
+++ magnum/api/config.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-#
 # Copyright 2013 - Noorul Islam K M
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
