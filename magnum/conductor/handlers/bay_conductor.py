5281705ec2e4b20da71b8c571940458662fcbd95
Tennyson53 <tennyson0503@hotmail.com>
2015-09-28 13:54:07 +0800

    modify bay_conductor

--- magnum/conductor/handlers/bay_conductor.py
+++ magnum/conductor/handlers/bay_conductor.py
@@ -93,9 +93,6 @@ def _create_stack(context, osc, bay, bay_create_timeout):
 
     return created_stack
 
-def _create_senlin_stack(context, osc, bay, bay_create_timeout):
-    
-
 
 def _update_stack(context, osc, bay, scale_manager=None):
     template_path, heat_params = _extract_template_definition(
@@ -127,20 +124,21 @@ class Handler(object):
 
         try:
             # Generate certificate and set the cert reference to bay
-            #senlinfunc.create_cluster(osc)
+            senlinfunc.create_cluster(osc)
             cert_manager.generate_certificates_to_bay(bay)
-            created_stack = _create_senlin_stack(context, osc, bay,
-                                                 bay_create_timeout)
+            #created_stack = _create_senlin_stack(context, osc, bay,
+            #                                     bay_create_timeout)
         except exc.HTTPBadRequest as e:
             cert_manager.delete_certificates_from_bay(bay)
             raise exception.InvalidParameterValue(message=str(e))
         except Exception:
             raise
 
-        bay.stack_id = created_stack['stack']['id']
+        #bay.stack_id = created_stack['stack']['id']
+        bay.stack_id = 'SUR_cluster'
         bay.create()
 
-        self._poll_and_check(osc, bay)
+        #self._poll_and_check(osc, bay)
 
         return bay
 
