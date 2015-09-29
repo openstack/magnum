832aa604363aebef178129a84d05a6b71de48cae
Eli Qiao <liyong.qiao@intel.com>
2015-09-29 14:58:04 +0800

    Swarm: Set to CREATE_FAILED status if swarm services not started
    This patch checks $NODE_SERVICES status before send SUCCESS single to heat cfn,
    this will make sure the bay is usable.
    
    Change-Id: Ie232c578c5c27b1842965bdda481096fb0b5c820
    Closes-Bug: #1500291

--- magnum/templates/docker-swarm/fragments/cfn-signal.sh
+++ magnum/templates/docker-swarm/fragments/cfn-signal.sh
@@ -3,8 +3,29 @@
 . /etc/sysconfig/heat-params
 
 echo "notifying heat"
+
+STATUS="SUCCESS"
+REASON="Setup complete"
+DATA="OK"
+FAILED_SERVICE=""
+
+for service in $NODE_SERVICES; do
+    echo "checking service status for $service"
+    systemctl status $service
+    if [[ $? -ne 0 ]]; then
+        echo "$service is not active, the cluster is not valid"
+        FAILED_SERVICE="$FAILED_SERVICE $service"
+    fi
+done
+
+if [[ -n $FAILED_SERVICE ]]; then
+    STATUS="FAILURE"
+    REASON="Setup failed, $FAILED_SERVICE not start up correctly."
+    DATA="Failed"
+fi
+
+data=$(echo '{"Status": "'${STATUS}'", "Reason": "'$REASON'", "Data": "'${DATA}'", "UniqueId": "00000"}')
+
 curl -sf -X PUT -H 'Content-Type: application/json' \
-	--data-binary '{"Status": "SUCCESS",
-	"Reason": "Setup complete",
-	"Data": "OK", "UniqueId": "00000"}' \
-	"$WAIT_HANDLE"
+    --data-binary "$data" \
+    "$WAIT_HANDLE"
