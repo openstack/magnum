d44d1563ff9b4d30ba29d5442db46d00ce45c879
mathspanda <mathspanda826@gmail.com>
2015-10-15 18:11:46 +0800

    add log metrics

--- magnum/sur/common/cadvisor_utils.py
+++ magnum/sur/common/cadvisor_utils.py
@@ -1,5 +1,6 @@
 '''
 Created on Oct 13, 2015
+
 '''
 
 import requests
@@ -12,16 +13,34 @@ class CadvisorUtils(object):
         
     def __get_machine_info(self):
         machine_info_url = '%s/%s' % (self.url, 'machine')
-        machine_info = requests.get(machine_info_url).json()
+
+        machine_info = None
+        try:
+            machine_info = requests.get(machine_info_url).json()
+        except Exception as e:
+            pass
         return machine_info
     
     def get_machine_memory(self):
-        return self.__get_machine_info()['topology'][0]['memory']
+        info = self.__get_machine_info()
+        if info is None:
+            return None
+        return info['topology'][0]['memory']
     
     def __get_containers_stats(self, count=1):
         stats_url = '%s/%s?count=%s' % (self.url, 'stats', count)
-        stats_info = requests.get(stats_url).json()
+
+        stats_info = None
+        try:
+            stats_info = requests.get(stats_url).json()
+        except Exception as e:
+            pass
         return stats_info
     
     def get_containers_memory_usage(self):
-        return self.__get_containers_stats()['/'][0]['memory']['usage']
+        info = self.__get_containers_stats()
+        if info is None:
+            return None
+        return info['/'][0]['memory']['usage']
+
+    
--- magnum/sur/monitor.py
+++ magnum/sur/monitor.py
@@ -1,18 +1,38 @@
-from magnum.sur.common.cadvisor_utils import CadvisorUtils
+from sur.common.cadvisor_utils import CadvisorUtils
+
+from oslo_log import log
+
+
+LOG = log.getLogger(__name__)
 
 
 def pull_data(address_list):
-
+    
     total_memory = 0.0
     used_memory = 0.0
 
+    lost_flag = False
+    
     for address in address_list:
         utils = CadvisorUtils(host=address)
 
-        total_memory += utils.get_machine_memory()
-        used_memory += utils.get_containers_memory_usage()
+        total_memory_info = utils.get_machine_memory()
+        used_memory_info = utils.get_containers_memory_usage()
 
-    if total_memory == 0.0:
+        if total_memory_info is None or used_memory_info is None:
+            lost_flag = True
+            LOG.warn('Cannot pull metrics from %s. '
+                     'cAdvisor is still pulling.' % address)
+            break
+
+        LOG.info('Pull metrics from %s successfully. [Total Memory=%s, '
+                 'Usage Memory=%s]' % (total_memory_info, used_memory_info))
+        
+        total_memory += total_memory_info
+        used_memory += used_memory_info
+
+    if lost_flag or total_memory == 0.0:
         return 0.0
-
+    
     return used_memory / total_memory * 100
+
