64c549653623d1e1c8cb471d22ac86e48fabdfbb
Hongbin Lu <hongbin.lu@huawei.com>
2015-11-22 23:43:00 -0500

    Enable HA mode for mesos bay in Heat templates
    Support a cluster of master nodes, which tolerates failure of no more
    than half number of nodes. The setup requires manually write down
    IP addresses of all master nodes, which is impossible by using
    cloud-init. Therefore, it replaces cloud-init with
    SoftwareDeploymentGroup.
    
    Using SoftwareDeployment requires to build additional packages into
    the image [1]. The image building instruction has been updated for
    that.
    
    [1] http://docs.openstack.org/developer/heat/template_guide/
    software_deployment.html#software-deployment-resources
    
    Partially-Implements: blueprint mesos-multi-master-node
    Change-Id: I34b85680803ea6070df94a6bccee3d677b8bfaab

--- magnum/templates/mesos/fragments/configure-mesos-master.sh
+++ magnum/templates/mesos/fragments/configure-mesos-master.sh
@@ -1,32 +1,72 @@
-#!/bin/sh
+#!/bin/bash
 
 . /etc/sysconfig/heat-params
 
-echo "configuring mesos (master)"
-mkdir -p /etc/marathon/conf
+echo "Configuring mesos (master)"
 
-# Set a ID for each master node
-echo "1" > /etc/zookeeper/conf/myid
+myip=$(ip addr show eth0 |
+       awk '$1 == "inet" {print $2}' | cut -f1 -d/)
 
-# Append server IP address(es)
-echo "
-server.1=$MESOS_MASTER_IP:2888:3888
-" >> /etc/zookeeper/conf/zoo.cfg
+# Fix /etc/hosts
+sed -i "s/127.0.1.1/$myip/" /etc/hosts
 
-# List of Zookeeper URLs
-echo "zk://$MESOS_MASTER_IP:2181/mesos" > /etc/mesos/zk
-echo "zk://$MESOS_MASTER_IP:2181/mesos" > /etc/marathon/conf/master
-echo "zk://$MESOS_MASTER_IP:2181/marathon" > /etc/marathon/conf/zk
+######################################################################
+#
+# Configure ZooKeeper
+#
+
+# List all ZooKeeper nodes
+id=1
+for master_ip in $MESOS_MASTERS_IPS; do
+    echo "server.$((id++))=${master_ip}:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+done
+
+# Set a ID for this node
+id=1
+for master_ip in $MESOS_MASTERS_IPS; do
+    if [ "$master_ip" = "$myip" ]; then
+        break
+    fi
+    id=$((id+1))
+done
+echo "$id" > /etc/zookeeper/conf/myid
+
+######################################################################
+#
+# Configure Mesos
+#
+
+# Set the ZooKeeper URL
+zk="zk://"
+for master_ip in $MESOS_MASTERS_IPS; do
+    zk="${zk}${master_ip}:2181,"
+done
+# Remove tailing ',' (format: zk://host1:port1,...,hostN:portN/path)
+zk=${zk::-1}
+echo "${zk}/mesos" > /etc/mesos/zk
+
+# The IP address to listen on
+echo "$myip" > /etc/mesos-master/ip
 
 # The size of the quorum of replicas
-echo "1" > /etc/mesos-master/quorum
+echo "$QUORUM" > /etc/mesos-master/quorum
 
-# The hostname the master should advertise in ZooKeeper
-echo "$MESOS_MASTER_IP" > /etc/mesos-master/hostname
-echo "$MESOS_MASTER_IP" > /etc/marathon/conf/hostname
+# The hostname advertised in ZooKeeper
+echo "$myip" > /etc/mesos-master/hostname
 
-# The IP address to listen on
-echo "$MESOS_MASTER_IP" > /etc/mesos-master/ip
-
-# The IP address to listen on
+# The cluster name
 echo "$CLUSTER_NAME" > /etc/mesos-master/cluster
+
+######################################################################
+#
+# Configure Marathon
+#
+
+mkdir -p /etc/marathon/conf
+
+# Set the ZooKeeper URL
+echo "${zk}/mesos" > /etc/marathon/conf/master
+echo "${zk}/marathon" > /etc/marathon/conf/zk
+
+# Set the hostname advertised in ZooKeeper
+echo "$myip" > /etc/marathon/conf/hostname
