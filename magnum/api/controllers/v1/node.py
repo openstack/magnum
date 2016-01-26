3732045c447367f4a3f73c581acfe86635d3a2b0
Vilobh Meshram <vilobhmm@yahoo-inc.com>
2016-01-25 16:33:23 -0800

    Fix typo in comment of several files
    Typo in the spelling of 'against' (written as 'agaist') is observed
    in the comment of several files. Not everything was covered as part of
    Idefb5d70dba554e2ea732af9d53f4bbe7e49e557.
    
    Change-Id: Ie2ac2be3e18afdf644e02288f13f10c10f6bc723

--- magnum/api/controllers/v1/node.py
+++ magnum/api/controllers/v1/node.py
@@ -174,7 +174,7 @@ class NodesController(rest.RestController):
         :param sort_key: column to sort results by. Default: id.
         :param sort_dir: direction to sort. "asc" or "desc". Default: asc.
         """
-        # NOTE(lucasagomes): /detail should only work agaist collections
+        # NOTE(lucasagomes): /detail should only work against collections
         parent = pecan.request.path.split('/')[:-1][-1]
         if parent != "nodes":
             raise exception.HTTPNotFound
