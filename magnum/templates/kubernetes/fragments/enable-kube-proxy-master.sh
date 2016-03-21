392a84ad407c20a97f449d2706004fa8ec8082cc
wangqun <bjwqun@cn.ibm.com>
2016-03-21 10:14:39 +0000

    Make kubernetes image version united into a variable
    Now kubernetes image is 1.0.6 and written in so many file.
    It is difficult to modify the parameter when we need to change
    it.
    
    The patch make the value united a variable.
    
    Change-Id: Ic476ffc90e4379e5a978c96dcc0f745e8d7fddd8

--- magnum/templates/kubernetes/fragments/enable-kube-proxy-master.sh
+++ magnum/templates/kubernetes/fragments/enable-kube-proxy-master.sh
@@ -17,7 +17,7 @@ spec:
   hostNetwork: true
   containers:
   - name: kube-proxy
-    image: gcr.io/google_containers/hyperkube:v1.0.6
+    image: gcr.io/google_containers/hyperkube:${KUBE_VERSION}
     command:
     - /hyperkube
     - proxy
