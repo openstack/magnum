c0604fbdca5020f4f6642f2a63299db2aa50c027
Hieu LE <hieulq@vn.fujitsu.com>
2016-08-10 18:01:58 +0700

    Improve unit test coverage for common/service.py
    Add new unit tests for common/service.py.
    Increase the coverage for common/service.py from 50 to 100%.
    
    Change-Id: I251d1541ae8088c47618fd0e11d81f44dc717aba
    Partial-Bug: #1511667

---
+++ magnum/tests/unit/common/test_service.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2016 OpenStack Foundation
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from oslo_log import log as logging
+
+from magnum.common import service
+from magnum.tests import base
+
+
+class TestMagnumService(base.BaseTestCase):
+
+    @mock.patch.object(logging, 'register_options')
+    @mock.patch.object(logging, 'setup')
+    @mock.patch('magnum.common.config.set_config_defaults')
+    @mock.patch('magnum.common.config.parse_args')
+    def test_prepare_service_with_argv_not_none(self, mock_parse, mock_set,
+                                                mock_setup, mock_reg):
+        argv = 'foo'
+        mock_parse.side_effect = lambda *args, **kwargs: None
+
+        service.prepare_service(argv)
+
+        mock_parse.assert_called_once_with(argv)
+        mock_setup.assert_called_once_with(base.CONF, 'magnum')
+        mock_reg.assert_called_once_with(base.CONF)
+        mock_set.assert_called_once_with()
+
+    @mock.patch.object(logging, 'register_options')
+    @mock.patch.object(logging, 'setup')
+    @mock.patch('magnum.common.config.set_config_defaults')
+    @mock.patch('magnum.common.config.parse_args')
+    def test_prepare_service_with_argv_none(self, mock_parse, mock_set,
+                                            mock_setup, mock_reg):
+        argv = None
+        mock_parse.side_effect = lambda *args, **kwargs: None
+
+        service.prepare_service(argv)
+
+        mock_parse.assert_called_once_with([])
+        mock_setup.assert_called_once_with(base.CONF, 'magnum')
+        mock_reg.assert_called_once_with(base.CONF)
+        mock_set.assert_called_once_with()
