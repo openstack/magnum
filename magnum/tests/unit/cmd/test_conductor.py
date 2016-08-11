81771f60b00d605dfe1bc07f1af6660cd3c1e0f2
Hieu LE <hieulq@vn.fujitsu.com>
2016-08-11 14:21:44 +0700

    Improve unit test coverage for cmd/conductor.py
    Add new unit tests for cmd/conductor.py.
    Increase the coverage for cmd/conductor.py from 0 to 100%.
    
    Change-Id: I0c65ee9bc161046bcbd2b0ef5a8faf2f50a0f43e
    Partial-Bug: #1511667

---
+++ magnum/tests/unit/cmd/test_conductor.py
@@ -0,0 +1,36 @@
+# Copyright 2016 - Fujitsu, Ltd.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import mock
+
+from magnum.cmd import conductor
+from magnum.tests import base
+
+
+class TestMagnumConductor(base.TestCase):
+
+    @mock.patch('oslo_service.service.launch')
+    @mock.patch.object(conductor, 'rpc_service')
+    @mock.patch('magnum.common.service.prepare_service')
+    def test_conductor(self, mock_prep, mock_rpc, mock_launch):
+        conductor.main()
+
+        server = mock_rpc.Service.create.return_value
+        launcher = mock_launch.return_value
+        mock_prep.assert_called_once_with(mock.ANY)
+        mock_rpc.Service.create.assert_called_once_with(
+            base.CONF.conductor.topic,
+            mock.ANY, mock.ANY, binary='magnum-conductor')
+        mock_launch.assert_called_once_with(base.CONF, server)
+        launcher.wait.assert_called_once_with()
