7c0be2d3840bcfbaa5b3e13898110cc945896833
Mathieu Velten <mathieu.velten@cern.ch>
2016-09-05 15:23:00 +0200

    Add support for overlay networks in Swarm.
    Built-in in docker >= 1.9, it justs need a K-V store.
    This patch reuses the existing etcd supporting Swarm.
    
    Change-Id: Iae2e5aa1d8836d344f16ec911c766c426b1b558a

--- magnum/drivers/swarm_fedora_atomic_v1/templates/fragments/network-service.sh
+++ magnum/drivers/swarm_fedora_atomic_v1/templates/fragments/network-service.sh
@@ -4,6 +4,13 @@
 
 echo "Configuring ${NETWORK_DRIVER} network service ..."
 
+if [ "$NETWORK_DRIVER" == "docker" ]; then
+    DOCKER_NETWORK_OPTIONS="--cluster-store etcd://$ETCD_SERVER_IP:2379\
+ --cluster-advertise $SWARM_NODE_IP:9379"
+    sed -i "/^DOCKER_NETWORK_OPTIONS=/ s#=.*#='$DOCKER_NETWORK_OPTIONS'#" \
+        /etc/sysconfig/docker-network
+fi
+
 if [ "$NETWORK_DRIVER" != "flannel" ]; then
     exit 0
 fi
