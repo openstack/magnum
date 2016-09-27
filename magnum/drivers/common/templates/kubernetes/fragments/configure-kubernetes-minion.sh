a10b53fa77b63a3b00ad30bd074ee283e0846b17
Ton Ngo <ton@us.ibm.com>
2016-09-27 04:37:21 +0000

    Backport fix for K8s load balancer with LBaaS v1
    Fix node name and auth_url
    
    Backport to stable/newton.
    Update the url to Keystone v2 which has been changed.
    The name of the node registered in the kube-apiserver
    was also changed at some point to use the IP instead of the
    Nova instance name as was done originally, and this
    breaks the Kubernetes plugin code.  Change the node name
    back to the Nova instance name in the option
    --hostname-override for kubelet.
    
    Some update to the document.
    
    With this patch, the load balancer works with Magnum Newton
    and later, along with the image fedora-atomic-latest.
    Important notes:
    1. The current image has Kubernetes release 1.2 and this only
    works with neutron LBaaS v1.  Support for LBaaS v2 requires
    Kubernetes release 1.3 or later.  Magnum support for 1.3
    is still in development.
    2. LBaaS v1 has been removed in Newton and is only available
    in Mitaka or by custom installation (likely requires some hacking).
    This means to get the load balancer feature, you will want to
    install Openstack Mitaka and Magnum Newton.
    
    Change-Id: Iff51f4e667663c217b6365a75d502e64c9420629
    Closes-Bug: #1524025

--- magnum/drivers/common/templates/kubernetes/fragments/configure-kubernetes-minion.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/configure-kubernetes-minion.sh
@@ -25,8 +25,14 @@ sed -i '
   /^KUBE_MASTER=/ s|=.*|="--master='"$KUBE_MASTER_URI"'"|
 ' /etc/kubernetes/config
 
+# NOTE:  Kubernetes plugin for Openstack requires that the node name registered
+# in the kube-apiserver be the same as the Nova name of the instance, so that
+# the plugin can use the name to query for attributes such as IP, etc.
+# The hostname of the node is set to be the Nova name of the instance, and
+# the option --hostname-override for kubelet uses the hostname to register the node.
+# Using any other name will break the load balancer and cinder volume features.
 HOSTNAME=$(hostname --short | sed 's/\.novalocal//')
-KUBELET_ARGS="--config=/etc/kubernetes/manifests --cadvisor-port=4194 ${KUBE_CONFIG}"
+KUBELET_ARGS="--config=/etc/kubernetes/manifests --cadvisor-port=4194 ${KUBE_CONFIG} --hostname-override=${HOSTNAME}"
 
 if [ -n "${INSECURE_REGISTRY_URL}" ]; then
     KUBELET_ARGS="${KUBELET_ARGS} --pod-infra-container-image=${INSECURE_REGISTRY_URL}/google_containers/pause\:0.8.0"
@@ -37,7 +43,7 @@ sed -i '
   /^KUBELET_ADDRESS=/ s/=.*/="--address=0.0.0.0"/
   /^KUBELET_HOSTNAME=/ s/=.*/=""/
   /^KUBELET_API_SERVER=/ s|=.*|="--api_servers='"$KUBE_MASTER_URI"'"|
-  /^KUBELET_ARGS=/ s|=.*|="'"${KUBELET_ARGS}"' --hostname-override='"${KUBE_NODE_IP}"'"|
+  /^KUBELET_ARGS=/ s|=.*|="'"${KUBELET_ARGS}"'"|
 ' /etc/kubernetes/kubelet
 
 sed -i '
@@ -75,7 +81,7 @@ tenant-name=$TENANT_NAME
 EOF
 
 cat << _EOC_ >> /etc/kubernetes/kubelet
-#KUBELET_ARGS="$KUBELET_ARGS --hostname-override=${HOSTNAME} --cloud-provider=openstack --cloud-config=/etc/kubernetes/kube_openstack_config"
+#KUBELET_ARGS="$KUBELET_ARGS --cloud-provider=openstack --cloud-config=/etc/kubernetes/kube_openstack_config"
 _EOC_
 
     if [ ! -f /usr/bin/udevadm ]; then
