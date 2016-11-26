4fa049e03741cbfa083c8f7f4f53b27dde8dc64d
Spyros Trigazis <strigazi@gmail.com>
2016-11-26 17:10:00 +0100

    [k8s_fedora_atomic] Remove podmaster
    Podmaster is deprecated since k8s 1.2 and its docker
    image is v1, incompatible with docker >=1.12.
    
    * Remove podmaster pod
    * Update manifests of kube-controller-manager and kube-scheduler
    * Rename SoftwareConfig to reflect the new functionality
    
    Closes-Bug: #1646109
    Change-Id: Ibf4ce06cbf5b79a4241c58c67b13a7c68145d3ae

--- magnum/drivers/common/templates/kubernetes/fragments/enable-kube-controller-manager-scheduler.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/enable-kube-controller-manager-scheduler.sh
@@ -3,67 +3,13 @@
 . /etc/sysconfig/heat-params
 
 if [ -n "${INSECURE_REGISTRY_URL}" ]; then
-    PODMASTER_IMAGE="${INSECURE_REGISTRY_URL}/google_containers/podmaster:1.1"
     HYPERKUBE_IMAGE="${INSECURE_REGISTRY_URL}/google_containers/hyperkube:${KUBE_VERSION}"
 else
-    PODMASTER_IMAGE="gcr.io/google_containers/podmaster:1.1"
     HYPERKUBE_IMAGE="gcr.io/google_containers/hyperkube:${KUBE_VERSION}"
 fi
 
 
 init_templates () {
-    local TEMPLATE=/etc/kubernetes/manifests/kube-podmaster.yaml
-    [ -f ${TEMPLATE} ] || {
-        echo "TEMPLATE: $TEMPLATE"
-        mkdir -p $(dirname ${TEMPLATE})
-        cat << EOF > ${TEMPLATE}
-apiVersion: v1
-kind: Pod
-metadata:
-  name: kube-podmaster
-  namespace: kube-system
-spec:
-  hostNetwork: true
-  containers:
-  - name: scheduler-elector
-    image: ${PODMASTER_IMAGE}
-    command:
-    - /podmaster
-    - --etcd-servers=http://127.0.0.1:2379
-    - --key=scheduler
-    - --source-file=/src/manifests/kube-scheduler.yaml
-    - --dest-file=/dst/manifests/kube-scheduler.yaml
-    volumeMounts:
-    - mountPath: /src/manifests
-      name: manifest-src
-      readOnly: true
-    - mountPath: /dst/manifests
-      name: manifest-dst
-  - name: controller-manager-elector
-    image: ${PODMASTER_IMAGE}
-    command:
-    - /podmaster
-    - --etcd-servers=http://127.0.0.1:2379
-    - --key=controller
-    - --source-file=/src/manifests/kube-controller-manager.yaml
-    - --dest-file=/dst/manifests/kube-controller-manager.yaml
-    terminationMessagePath: /dev/termination-log
-    volumeMounts:
-    - mountPath: /src/manifests
-      name: manifest-src
-      readOnly: true
-    - mountPath: /dst/manifests
-      name: manifest-dst
-  volumes:
-  - hostPath:
-      path: /srv/kubernetes/manifests
-    name: manifest-src
-  - hostPath:
-      path: /etc/kubernetes/manifests
-    name: manifest-dst
-EOF
-    }
-
     local SERVICE_ACCOUNT_PRIVATE_KEY_FILE=/etc/kubernetes/ssl/server.key
     local ROOT_CA_FILE=/etc/kubernetes/ssl/ca.crt
 
@@ -72,7 +18,7 @@ EOF
         ROOT_CA_FILE=
     fi
 
-    local TEMPLATE=/srv/kubernetes/manifests/kube-controller-manager.yaml
+    local TEMPLATE=/etc/kubernetes/manifests/kube-controller-manager.yaml
     [ -f ${TEMPLATE} ] || {
         echo "TEMPLATE: $TEMPLATE"
         mkdir -p $(dirname ${TEMPLATE})
@@ -124,7 +70,7 @@ spec:
 EOF
     }
 
-    local TEMPLATE=/srv/kubernetes/manifests/kube-scheduler.yaml
+    local TEMPLATE=/etc/kubernetes/manifests/kube-scheduler.yaml
     [ -f ${TEMPLATE} ] || {
         echo "TEMPLATE: $TEMPLATE"
         mkdir -p $(dirname ${TEMPLATE})
