7c35c8fe40ec2b012696965e225d2b2d6ea0f6b1
Ricardo Rocha <rocha.porto@gmail.com>
2017-05-03 16:46:06 +0200

    Add CoreDNS deployment in kubernetes atomic
    Enable internal cluster DNS by deploying CoreDNS in the kube-system
    namespace. It covers dns queries for both the cluster and external,
    acting as a proxy with a cache layer in front.
    
    Version of CoreDNS hard-coded to 007, image taken from dockerhub.
    
    Related-Bug: #1692449
    
    Change-Id: I0a9703b531fe872416dcd79fa7d4d27c1ea61586

---
+++ magnum/drivers/common/templates/kubernetes/fragments/core-dns-service.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+
+. /etc/sysconfig/heat-params
+
+CORE_DNS=/etc/kubernetes/manifests/kube-coredns.yaml
+[ -f ${CORE_DNS} ] || {
+    echo "Writing File: $CORE_DNS"
+    mkdir -p $(dirname ${CORE_DNS})
+    cat << EOF > ${CORE_DNS}
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: coredns
+  namespace: kube-system
+data:
+  Corefile: |
+    .:53 {
+        errors
+        log stdout
+        health
+        kubernetes ${DNS_CLUSTER_DOMAIN} {
+          cidrs ${PORTAL_NETWORK_CIDR}
+        }
+        proxy . /etc/resolv.conf
+        cache 30
+    }
+---
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: coredns
+  namespace: kube-system
+  labels:
+    k8s-app: coredns
+    kubernetes.io/cluster-service: "true"
+    kubernetes.io/name: "CoreDNS"
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      k8s-app: coredns
+  template:
+    metadata:
+      labels:
+        k8s-app: coredns
+      annotations:
+        scheduler.alpha.kubernetes.io/critical-pod: ''
+        scheduler.alpha.kubernetes.io/tolerations: '[{"key":"CriticalAddonsOnly", "operator":"Exists"}]'
+    spec:
+      containers:
+      - name: coredns
+        image: coredns/coredns:007
+        imagePullPolicy: Always
+        args: [ "-conf", "/etc/coredns/Corefile" ]
+        volumeMounts:
+        - name: config-volume
+          mountPath: /etc/coredns
+        ports:
+        - containerPort: 53
+          name: dns
+          protocol: UDP
+        - containerPort: 53
+          name: dns-tcp
+          protocol: TCP
+        livenessProbe:
+          httpGet:
+            path: /health
+            port: 8080
+            scheme: HTTP
+          initialDelaySeconds: 60
+          timeoutSeconds: 5
+          successThreshold: 1
+          failureThreshold: 5
+      dnsPolicy: Default
+      volumes:
+        - name: config-volume
+          configMap:
+            name: coredns
+            items:
+            - key: Corefile
+              path: Corefile
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: kube-dns
+  namespace: kube-system
+  labels:
+    k8s-app: coredns
+    kubernetes.io/cluster-service: "true"
+    kubernetes.io/name: "CoreDNS"
+spec:
+  selector:
+    k8s-app: coredns
+  clusterIP: ${DNS_SERVICE_IP}
+  ports:
+  - name: dns
+    port: 53
+    protocol: UDP
+  - name: dns-tcp
+    port: 53
+    protocol: TCP
+EOF
+}
+
+echo "Waiting for Kubernetes API..."
+until curl --silent "http://127.0.0.1:8080/version"
+do
+    sleep 5
+done
+
+kubectl create --validate=false -f $CORE_DNS
