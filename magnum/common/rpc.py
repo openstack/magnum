ed39dd8b49203df8f7ddc1bf2bfd245abfd5a5e2
rajat29 <rajat.sharma@nectechnologies.in>
2017-06-07 10:47:38 +0530

    Use get_rpc_transport instead of get_transport
    With the added get_rpc_transport() function in oslo.messaging [1]
    and the recent version bump of oslo.messaging to 5.24.2, we can
    safely replace get_transport() with get_rpc_transport() which is much
    cleaner.
    
    [1] https://review.openstack.org/#/c/454194/
    Related-Bug: #1680192
    
    Change-Id: I3a3e88f0fc85a03c0ee9b82edd165436f136a4d0

--- magnum/common/rpc.py
+++ magnum/common/rpc.py
@@ -52,8 +52,8 @@ EXTRA_EXMODS = []
 def init(conf):
     global TRANSPORT, NOTIFIER
     exmods = get_allowed_exmods()
-    TRANSPORT = messaging.get_transport(conf,
-                                        allowed_remote_exmods=exmods)
+    TRANSPORT = messaging.get_rpc_transport(conf,
+                                            allowed_remote_exmods=exmods)
     serializer = RequestContextSerializer(JsonPayloadSerializer())
     NOTIFIER = messaging.Notifier(TRANSPORT, serializer=serializer)
 
