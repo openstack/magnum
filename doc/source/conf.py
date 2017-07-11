e66720f0635193ee6da7e8120bc5fe53987d4b7c
Madhuri Kumari <madhuri.kumari@intel.com>
2017-07-11 16:48:16 +0530

    Add warning-is-error in setup.cfg
    Change-Id: Iaa73fa99c3fc6aee030735e853542339bbf05384

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,6 +21,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.graphviz',
     'stevedore.sphinxext',
     'oslosphinx'
 ]
