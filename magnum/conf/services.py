e8ac68b33b3b7bf54baa36b89ac90e9e5a666599
lingyongxu <lyxu@fiberhome.com>
2017-07-27 15:31:55 +0800

    Use HostAddressOpt for opts that accept IP and hostnames
    Some configuration options were accepting both IP addresses
    and hostnames. Since there was no specific OSLO opt type to
    support this, we were using ``StrOpt``. The change [1] that
    added support for ``HostAddressOpt`` type was merged in Ocata
    and became available for use with oslo version 3.22.
    
    This patch changes the opt type of configuration options to use
    this more relevant opt type - HostAddressOpt.
    
    [1] I77bdb64b7e6e56ce761d76696bc4448a9bd325eb
    
    Change-Id: Id179ad55d4344a7dc2214896290890862b560e0c

--- magnum/conf/services.py
+++ magnum/conf/services.py
@@ -15,12 +15,13 @@ from oslo_config import cfg
 from magnum.i18n import _
 
 service_opts = [
-    cfg.StrOpt('host',
-               help=_('Name of this node. This can be an opaque identifier. '
-                      'It is not necessarily a hostname, FQDN, or IP address. '
-                      'However, the node name must be valid within '
-                      'an AMQP key, and if using ZeroMQ, a valid '
-                      'hostname, FQDN, or IP address.')),
+    cfg.HostAddressOpt('host',
+                       help=_('Name of this node. This can be an opaque '
+                              'identifier. It is not necessarily a hostname, '
+                              'FQDN, or IP address. However, the node name '
+                              'must be valid within an AMQP key, and if using '
+                              'ZeroMQ, a valid hostname, FQDN, or IP '
+                              'address.')),
 ]
 
 
