9aacda8f8315e69fd67f173a6460259f847bad2c
Spyros Trigazis <spyridon.trigazis@cern.ch>
2017-12-15 16:57:52 +0000

    Leverage heat-container-agent for monitoring
    Use the heat-container-agent from a system container.
    It means that the docker daemon can be started later.
    
    Pass as a software deployment with the heat-agent the following
    software-configurations:
    * prometheus-monitoring
    ** pin prometheus to v1.8.2 since its config is not 2.0.0
       compatible
    
    Add heat-container-agent container image.
    
    Implements: blueprint heat-agent
    Related-Bug: #1680900
    Change-Id: I084b7fe51eddb7b36c74f9fe76cda37e8b48f646
    (cherry picked from commit 273405cb5a8cdbcec816f646cfe82f13784b3bf1)

---
+++ magnum/drivers/common/templates/kubernetes/fragments/start-container-agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. /etc/sysconfig/heat-params
+
+set -ux
+
+_prefix=${CONTAINER_INFRA_PREFIX:-docker.io/openstackmagnum/}
+atomic install \
+--storage ostree \
+--system \
+--system-package no \
+--set REQUESTS_CA_BUNDLE=/etc/pki/tls/certs/ca-bundle.crt \
+--name heat-container-agent \
+${_prefix}heat-container-agent:rawhide
+
+systemctl start heat-container-agent
