b8f6261f44e0f7bfcb04ffb6af6c16c041eb8dcd
Spyros Trigazis <strigazi@gmail.com>
2018-04-25 12:22:43 +0000

    k8s_fedora: Add admin user
    Add an admin service account and give it the
    cluster role. It can be used for access apps
    with token authentication like the
    kubernetes-dashboard.
    
    Remove the cluster role from the dashboard service account.
    
    Change-Id: I7980c0e72b0d71921e42af7338d02b8a1e563c34
    Closes-Bug: #1766284
    (cherry picked from commit 91d5229b9c0e083cae60a8dc3e546145a82c7f93)

--- magnum/drivers/common/templates/kubernetes/fragments/kube-apiserver-to-kubelet-role.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/kube-apiserver-to-kubelet-role.sh
@@ -45,3 +45,31 @@ subjects:
     kind: User
     name: kubernetes
 EOF
+
+# Create an admin user and give it the cluster role.
+ADMIN_RBAC=/srv/magnum/kubernetes/kubernetes-admin-rbac.yaml
+
+[ -f ${ADMIN_RBAC} ] || {
+    echo "Writing File: $ADMIN_RBAC"
+    mkdir -p $(dirname ${ADMIN_RBAC})
+    cat << EOF > ${ADMIN_RBAC}
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: admin
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin
+  namespace: kube-system
+EOF
+}
