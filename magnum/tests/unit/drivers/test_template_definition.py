openstack/magnum

commit 8fb27da2fc39c8be32ec756c1cc3779ae07d30a4

Update coredns from upstream manifest and to 1.3.1
https://github.com/coredns/deployment/blob/5fe683c0579ed3b3f5d8a7ad5f85c2a9b88b3c8f/kubernetes/coredns.yaml.sed

story: 2003993
task: 30493

Change-Id: I0b0b4f98c20748c37c2d2f498ced222a53b52214
Signed-off-by: Spyros Trigazis <spyridon.trigazis@cern.ch>

magnum/drivers/heat/k8s_fedora_template_def.py
@@ -115,8 +115,10 @@ class K8sFedoraTemplateDefinition(k8s_template_def.K8sTemplateDefinition):
                 '"cinder" volume driver needs "cloud_provider_enabled" label '
                 'to be true or unset.'))
 
-        label_list = ['kube_tag', 'container_infra_prefix',
-                      'availability_zone', 'cgroup_driver',
+        label_list = ['coredns_tag',
+                      'kube_tag', 'container_infra_prefix',
+                      'availability_zone',
+                      'cgroup_driver',
                       'calico_tag', 'calico_cni_tag',
                       'calico_kube_controllers_tag', 'calico_ipv4pool',
                       'etcd_tag', 'flannel_tag', 'flannel_cni_tag',

magnum/drivers/k8s_fedora_atomic_v1/templates/kubecluster.yaml
@@ -351,6 +351,11 @@ parameters:
     description: tag of the etcd system container
     default: v3.2.7
 
+  coredns_tag:
+    type: string
+    description: tag for coredns
+    default: 1.3.1
+
   flannel_tag:
     type: string
     description: tag of the flannel container
@@ -850,6 +855,7 @@ resources:
           cloud_provider_enabled: {get_param: cloud_provider_enabled}
           kube_version: {get_param: kube_version}
           etcd_tag: {get_param: etcd_tag}
+          coredns_tag: {get_param: coredns_tag}
           flannel_tag: {get_param: flannel_tag}
           flannel_cni_tag: {get_param: flannel_cni_tag}
           kube_dashboard_version: {get_param: kube_dashboard_version}

magnum/drivers/k8s_fedora_atomic_v1/templates/kubemaster.yaml
@@ -239,6 +239,10 @@ parameters:
     type: string
     description: tag of the etcd system container
 
+  coredns_tag:
+    type: string
+    description: tag of the coredns container
+
   flannel_tag:
     type: string
     description: tag of the flannel system containers
@@ -551,6 +555,7 @@ resources:
             "$CLOUD_PROVIDER_TAG": {get_param: cloud_provider_tag}
             "$CLOUD_PROVIDER_ENABLED": {get_param: cloud_provider_enabled}
             "$ETCD_TAG": {get_param: etcd_tag}
+            "$COREDNS_TAG": {get_param: coredns_tag}
             "$FLANNEL_TAG": {get_param: flannel_tag}
             "$FLANNEL_CNI_TAG": {get_param: flannel_cni_tag}
             "$KUBE_VERSION": {get_param: kube_version}

magnum/tests/unit/drivers/test_template_definition.py
@@ -452,6 +452,7 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'etcd_volume_size')
         kube_tag = mock_cluster.labels.get('kube_tag')
         etcd_tag = mock_cluster.labels.get('etcd_tag')
+        coredns_tag = mock_cluster.labels.get('coredns_tag')
         flannel_tag = mock_cluster.labels.get('flannel_tag')
         flannel_cni_tag = mock_cluster.labels.get('flannel_cni_tag')
         container_infra_prefix = mock_cluster.labels.get(
@@ -559,6 +560,7 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'region_name': mock_osc.cinder_region_name.return_value,
             'kube_tag': kube_tag,
             'etcd_tag': etcd_tag,
+            'coredns_tag': coredns_tag,
             'flannel_tag': flannel_tag,
             'flannel_cni_tag': flannel_cni_tag,
             'container_infra_prefix': container_infra_prefix,
@@ -845,6 +847,7 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'etcd_volume_size')
         kube_tag = mock_cluster.labels.get('kube_tag')
         etcd_tag = mock_cluster.labels.get('etcd_tag')
+        coredns_tag = mock_cluster.labels.get('coredns_tag')
         flannel_tag = mock_cluster.labels.get('flannel_tag')
         flannel_cni_tag = mock_cluster.labels.get('flannel_cni_tag')
         container_infra_prefix = mock_cluster.labels.get(
@@ -954,6 +957,7 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'kubernetes_port': 8080,
             'kube_tag': kube_tag,
             'etcd_tag': etcd_tag,
+            'coredns_tag': coredns_tag,
             'flannel_tag': flannel_tag,
             'flannel_cni_tag': flannel_cni_tag,
             'container_infra_prefix': container_infra_prefix,

magnum/drivers/k8s_coreos_v1/templates/kubecluster.yaml
@@ -330,6 +330,11 @@ parameters:
     description: tag of the etcd system container
     default: v3.2.7
 
+  coredns_tag:
+    type: string
+    description: tag for coredns
+    default: 1.3.1
+
   flannel_tag:
     type: string
     description: tag of the flannel system containers
@@ -681,6 +686,7 @@ resources:
           kube_tag: {get_param: kube_tag}
           kube_version: {get_param: kube_version}
           etcd_tag: {get_param: etcd_tag}
+          coredns_tag: {get_param: coredns_tag}
           kube_dashboard_version: {get_param: kube_dashboard_version}
           trustee_user_id: {get_param: trustee_user_id}
           trustee_password: {get_param: trustee_password}

magnum/drivers/k8s_coreos_v1/templates/kubemaster.yaml
@@ -219,6 +219,10 @@ parameters:
     type: string
     description: tag of the etcd system container
 
+  coredns_tag:
+    type: string
+    description: tag for coredns
+
   kube_version:
     type: string
     description: version of kubernetes used for kubernetes cluster

magnum/drivers/common/templates/kubernetes/fragments/core-dns-service.sh
@@ -19,7 +19,7 @@ metadata:
   name: coredns
   namespace: kube-system
 ---
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRole
 metadata:
   labels:
@@ -36,8 +36,14 @@ rules:
   verbs:
   - list
   - watch
+- apiGroups:
+  - ""
+  resources:
+  - nodes
+  verbs:
+  - get
 ---
-apiVersion: rbac.authorization.k8s.io/v1beta1
+apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
   annotations:
@@ -66,45 +72,65 @@ data:
         log stdout
         health
         kubernetes ${DNS_CLUSTER_DOMAIN} ${PORTAL_NETWORK_CIDR} ${PODS_NETWORK_CIDR} {
-            pods verified
+           pods verified
+           upstream
+           fallthrough in-addr.arpa ip6.arpa
         }
         prometheus :9153
-        proxy . /etc/resolv.conf
+        forward . /etc/resolv.conf
         cache 30
+        loop
+        reload
+        loadbalance
     }
+
 ---
-apiVersion: extensions/v1beta1
+apiVersion: apps/v1
 kind: Deployment
 metadata:
   name: coredns
   namespace: kube-system
   labels:
-    k8s-app: coredns
+    k8s-app: kube-dns
     kubernetes.io/name: "CoreDNS"
 spec:
-  replicas: 1
+  replicas: 2
+  strategy:
+    type: RollingUpdate
+    rollingUpdate:
+      maxUnavailable: 1
   selector:
     matchLabels:
-      k8s-app: coredns
+      k8s-app: kube-dns
   template:
     metadata:
       labels:
-        k8s-app: coredns
+        k8s-app: kube-dns
     spec:
+      priorityClassName: system-cluster-critical
       serviceAccountName: coredns
       tolerations:
-        - key: node-role.kubernetes.io/master
-          effect: NoSchedule
         - key: "CriticalAddonsOnly"
           operator: "Exists"
+      nodeSelector:
+        beta.kubernetes.io/os: linux
       containers:
       - name: coredns
-        image: ${_dns_prefix}coredns:1.3.0
+        image: ${_dns_prefix}coredns:${COREDNS_TAG}
         imagePullPolicy: IfNotPresent
+        resources:
+          limits:
+            memory: 170Mi
+          requests:
+            cpu: 100m
+            memory: 70Mi
         args: [ "-conf", "/etc/coredns/Corefile" ]
         volumeMounts:
         - name: config-volume
           mountPath: /etc/coredns
+          readOnly: true
+        - name: tmp
+          mountPath: /tmp
         ports:
         - containerPort: 53
           name: dns
@@ -115,6 +141,14 @@ spec:
         - containerPort: 9153
           name: metrics
           protocol: TCP
+        securityContext:
+          allowPrivilegeEscalation: false
+          capabilities:
+            add:
+            - NET_BIND_SERVICE
+            drop:
+            - all
+          readOnlyRootFilesystem: true
         livenessProbe:
           httpGet:
             path: /health
@@ -124,8 +158,15 @@ spec:
           timeoutSeconds: 5
           successThreshold: 1
           failureThreshold: 5
+        readinessProbe:
+          httpGet:
+            path: /health
+            port: 8080
+            scheme: HTTP
       dnsPolicy: Default
       volumes:
+        - name: tmp
+          emptyDir: {}
         - name: config-volume
           configMap:
             name: coredns
@@ -138,13 +179,16 @@ kind: Service
 metadata:
   name: kube-dns
   namespace: kube-system
+  annotations:
+    prometheus.io/port: "9153"
+    prometheus.io/scrape: "true"
   labels:
-    k8s-app: coredns
+    k8s-app: kube-dns
     kubernetes.io/cluster-service: "true"
     kubernetes.io/name: "CoreDNS"
 spec:
   selector:
-    k8s-app: coredns
+    k8s-app: kube-dns
   clusterIP: ${DNS_SERVICE_IP}
   ports:
   - name: dns

magnum/drivers/common/templates/kubernetes/fragments/write-heat-params-master.yaml
@@ -47,6 +47,7 @@ write_files:
       CLOUD_PROVIDER_TAG="$CLOUD_PROVIDER_TAG"
       CLOUD_PROVIDER_ENABLED="$CLOUD_PROVIDER_ENABLED"
       ETCD_TAG="$ETCD_TAG"
+      COREDNS_TAG="$COREDNS_TAG"
       FLANNEL_TAG="$FLANNEL_TAG"
       FLANNEL_CNI_TAG="$FLANNEL_CNI_TAG"
       KUBE_VERSION="$KUBE_VERSION"

commit 41b83cef43c21ce3bb68ce0cd76867fd111abf36

[k8s] Update prometheus monitoring helm based configuration
* prometheus-operator chart version upgraded from 0.1.31. to 5.12.3
* Fix an issue where when using Feature Gate Priority the scheduler
would evict the prometheus monitoring node-exporter pods
* Fix an issue where intensive CPU utilization would make the
metrics fail intermitently or completly fail
* Prometheus resources are now calculated based on the MAX_NODE_COUNT
requested
* Change the sampling rate from the standard 30s to 1 minute (Rollback)
* Add the missing tiller CONTAINER_INFRA_PREFIX variable to the ConfigMap
* Add label prometheus_operator_chart_tag to enable the user to
specify the stable/prometheus-operator chart to use
* Fix breaking changes on CoreDNS metrics introduced by
https://github.com/openstack/magnum/commit/8fb27da2fc39c8be32ec756c1cc3779ae07d30a4
* Fix Graphana dashboard not showing data.


Change-Id: If42873cd6668c07e4e911e4eef5e4ae2232be66f
Task: 30777
Task: 30779
Story: 2005588
Signed-off-by: Diogo Guerra <dy090.guerra@gmail.com>

magnum/drivers/common/templates/kubernetes/fragments/write-heat-params-master.sh
@@ -40,6 +40,7 @@ HEAT_PARAMS=/etc/sysconfig/heat-params
       CLUSTER_UUID="$CLUSTER_UUID"
       MAGNUM_URL="$MAGNUM_URL"
       MONITORING_ENABLED="$MONITORING_ENABLED"
+      PROMETHEUS_OPERATOR_CHART_TAG="$PROMETHEUS_OPERATOR_CHART_TAG"
       VOLUME_DRIVER="$VOLUME_DRIVER"
       REGION_NAME="$REGION_NAME"
       HTTP_PROXY="$HTTP_PROXY"

magnum/drivers/common/templates/kubernetes/helm/prometheus-operator.sh
@@ -10,9 +10,15 @@ printf "Starting to run ${step}\n"
 ### Configuration
 ###############################################################################
 CHART_NAME="prometheus-operator"
-CHART_VERSION="0.1.31"
+CHART_VERSION=${PROMETHEUS_OPERATOR_CHART_TAG:-5.12.3}
+
 
 if [ "$(echo ${MONITORING_ENABLED} | tr '[:upper:]' '[:lower:]')" = "true" ]; then
+
+    # Calculate resources needed to run the Prometheus Monitoring Solution
+    # MAX_NODE_COUNT so we can have metrics even if cluster scales
+    PROMETHEUS_SERVER_CPU=$(expr 128 + 7 \* ${MAX_NODE_COUNT} )
+    PROMETHEUS_SERVER_RAM=$(expr 256 + 40 \* ${MAX_NODE_COUNT})
 
     # Validate if communication node <-> master is secure or insecure
     PROTOCOL="https"
@@ -53,11 +59,12 @@ data:
     done
     helm repo update
 
-    if [[ \$(helm history prometheus-operator | grep prometheus-operator) ]]; then
+    if [[ \$(helm history ${CHART_NAME} | grep ${CHART_NAME}) ]]; then
         echo "${CHART_NAME} already installed on server. Continue..."
         exit 0
     else
-        helm install stable/${CHART_NAME} --namespace monitoring --name ${CHART_NAME} --version v${CHART_VERSION} --values /opt/magnum/install-${CHART_NAME}-values.yaml
+        # TODO: Set namespace to monitoring. This is needed as the Kubernetes default priorityClass can only be used in NS kube-system
+        helm install stable/${CHART_NAME} --namespace kube-system --name ${CHART_NAME} --version v${CHART_VERSION} --values /opt/magnum/install-${CHART_NAME}-values.yaml
     fi
 
   install-${CHART_NAME}-values.yaml:  |
@@ -67,11 +74,22 @@ data:
     alertmanager:
       alertmanagerSpec:
         image:
-          repository: ${CONTAINER_INFRA_PREFIX:-quay.io/}prometheus/alertmanager
+          repository: ${CONTAINER_INFRA_PREFIX:-quay.io/prometheus/}alertmanager
+        # # Needs testing
+        # resources:
+        #   requests:
+        #     cpu: 100m
+        #     memory: 256Mi
+        priorityClassName: "system-cluster-critical"
+
 
     # Dashboard
     grafana:
       #enabled: ${ENABLE_GRAFANA}
+      resources:
+        requests:
+          cpu: 100m
+          memory: 128Mi
       adminPassword: ${ADMIN_PASSWD}
 
     kubeApiServer:
@@ -88,37 +106,59 @@ data:
         port: 9153
         targetPort: 9153
         selector:
-          k8s-app: coredns
+          k8s-app: kube-dns
 
     kubeEtcd:
-      service:
-        port: 4001
-        targetPort: 4001
-        selector:
-          k8s-app: etcd-server
       serviceMonitor:
         scheme: ${PROTOCOL}
-        insecureSkipVerify: ${INSECURE_SKIP_VERIFY}
+        insecureSkipVerify: true
         ##  If Protocol is http this files should be neglected
-        caFile: ${CERT_DIR}/ca.crt
-        certFile: ${CERT_DIR}/kubelet.crt
-        keyFile: ${CERT_DIR}/kubelet.key
+        caFile: /etc/prometheus/secrets/etcd-certificates/ca.crt
+        certFile: /etc/prometheus/secrets/etcd-certificates/kubelet.crt
+        keyFile: /etc/prometheus/secrets/etcd-certificates/kubelet.key
+
+    kube-state-metrics:
+      priorityClassName: "system-cluster-critical"
+      resources:
+        #Guaranteed
+        limits:
+          cpu: 50m
+          memory: 64M
+
+    prometheus-node-exporter:
+      priorityClassName: "system-node-critical"
+      resources:
+        #Guaranteed
+        limits:
+          cpu: 20m
+          memory: 20M
 
     prometheusOperator:
+      priorityClassName: "system-cluster-critical"
       image:
-        repository: ${CONTAINER_INFRA_PREFIX:-quay.io/}coreos/prometheus-operator
+        repository: ${CONTAINER_INFRA_PREFIX:-quay.io/coreos/}prometheus-operator
       configmapReloadImage:
-        repository: ${CONTAINER_INFRA_PREFIX:-quay.io/}coreos/configmap-reload
+        repository: ${CONTAINER_INFRA_PREFIX:-quay.io/coreos/}configmap-reload
       prometheusConfigReloaderImage:
-        repository: ${CONTAINER_INFRA_PREFIX:-quay.io/}coreos/prometheus-config-reloader
+        repository: ${CONTAINER_INFRA_PREFIX:-quay.io/coreos/}prometheus-config-reloader
       hyperkubeImage:
         repository: ${CONTAINER_INFRA_PREFIX:-gcr.io/google-containers/}hyperkube
 
     prometheus:
       prometheusSpec:
+        scrapeInterval: 30s
+        evaluationInterval: 30s
         image:
-          repository: ${CONTAINER_INFRA_PREFIX:-quay.io/}prometheus/prometheus
+          repository: ${CONTAINER_INFRA_PREFIX:-quay.io/prometheus/}prometheus
         retention: 14d
+        resources:
+          requests:
+            cpu: ${PROMETHEUS_SERVER_CPU}m
+            memory: ${PROMETHEUS_SERVER_RAM}M
+        # secrets:
+        # - etcd-certificates
+        priorityClassName: "system-cluster-critical"
+
 ---
 apiVersion: batch/v1
 kind: Job
@@ -132,7 +172,7 @@ spec:
       serviceAccountName: tiller
       containers:
       - name: config-helm
-        image: docker.io/openstackmagnum/helm-client:dev
+        image: ${CONTAINER_INFRA_PREFIX:-docker.io/openstackmagnum/}helm-client:dev
         command:
         - bash
         args:

magnum/drivers/heat/k8s_fedora_template_def.py
@@ -132,6 +132,7 @@ class K8sFedoraTemplateDefinition(k8s_template_def.K8sTemplateDefinition):
                       'heat_container_agent_tag',
                       'keystone_auth_enabled', 'k8s_keystone_auth_tag',
                       'monitoring_enabled',
+                      'prometheus_operator_chart_tag',
                       'tiller_enabled',
                       'tiller_tag',
                       'tiller_namespace',

magnum/drivers/k8s_fedora_atomic_v1/templates/kubecluster.yaml
@@ -577,6 +577,11 @@ parameters:
     description: Enable or disable prometheus-operator monitoring solution.
     default: false
 
+  prometheus_operator_chart_tag:
+    type: string
+    description: The stable/prometheus-operator chart version to use.
+    default: 5.12.3
+
   project_id:
     type: string
     description: >
@@ -929,6 +934,7 @@ resources:
           keystone_auth_enabled: {get_param: keystone_auth_enabled}
           k8s_keystone_auth_tag: {get_param: k8s_keystone_auth_tag}
           monitoring_enabled: {get_param: monitoring_enabled}
+          prometheus_operator_chart_tag: {get_param: prometheus_operator_chart_tag}
           project_id: {get_param: project_id}
           tiller_enabled: {get_param: tiller_enabled}
           tiller_tag: {get_param: tiller_tag}

magnum/drivers/k8s_fedora_atomic_v1/templates/kubemaster.yaml
@@ -430,6 +430,11 @@ parameters:
     description: Enable or disable prometheus-operator monitoring solution.
     default: false
 
+  prometheus_operator_chart_tag:
+    type: string
+    description: The stable/prometheus-operator chart version to use.
+    default: 5.12.3
+
   project_id:
     type: string
     description: >
@@ -613,6 +618,7 @@ resources:
                   "$KEYSTONE_AUTH_ENABLED": {get_param: keystone_auth_enabled}
                   "$K8S_KEYSTONE_AUTH_TAG": {get_param: k8s_keystone_auth_tag}
                   "$MONITORING_ENABLED": {get_param: monitoring_enabled}
+                  "$PROMETHEUS_OPERATOR_CHART_TAG": {get_param: prometheus_operator_chart_tag}
                   "$PROJECT_ID": {get_param: project_id}
                   "$EXTERNAL_NETWORK_ID": {get_param: external_network}
                   "$TILLER_ENABLED": {get_param: tiller_enabled}

releasenotes/notes/helm-install-prometheus-operator-ea87752bc57a0945.yaml
@@ -5,4 +5,4 @@ features:
     solution by means of helm stable/prometheus-operator public chart.
     Defaults to false. grafana_admin_passwd label can be used to set
     grafana dashboard admin access password. If grafana_admin_passwd
-    is not set the password defaults to prom_operator.
+    is not set the password defaults to admin.

magnum/tests/unit/drivers/test_template_definition.py
@@ -510,6 +510,8 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'k8s_keystone_auth_tag')
         monitoring_enabled = mock_cluster.labels.get(
             'monitoring_enabled')
+        prometheus_operator_chart_tag = mock_cluster.labels.get(
+            'prometheus_operator_chart_tag')
         project_id = mock_cluster.project_id
         tiller_enabled = mock_cluster.labels.get(
             'tiller_enabled')
@@ -589,6 +591,7 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'keystone_auth_enabled': keystone_auth_enabled,
             'k8s_keystone_auth_tag': k8s_keystone_auth_tag,
             'monitoring_enabled': monitoring_enabled,
+            'prometheus_operator_chart_tag': prometheus_operator_chart_tag,
             'project_id': project_id,
             'external_network': external_network_id,
             'tiller_enabled': tiller_enabled,
@@ -912,6 +915,8 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'k8s_keystone_auth_tag')
         monitoring_enabled = mock_cluster.labels.get(
             'monitoring_enabled')
+        prometheus_operator_chart_tag = mock_cluster.labels.get(
+            'prometheus_operator_chart_tag')
         project_id = mock_cluster.project_id
         tiller_enabled = mock_cluster.labels.get(
             'tiller_enabled')
@@ -993,6 +998,7 @@ class AtomicK8sTemplateDefinitionTestCase(BaseK8sTemplateDefinitionTestCase):
             'keystone_auth_enabled': keystone_auth_enabled,
             'k8s_keystone_auth_tag': k8s_keystone_auth_tag,
             'monitoring_enabled': monitoring_enabled,
+            'prometheus_operator_chart_tag': prometheus_operator_chart_tag,
             'project_id': project_id,
             'external_network': external_network_id,
             'tiller_enabled': tiller_enabled,