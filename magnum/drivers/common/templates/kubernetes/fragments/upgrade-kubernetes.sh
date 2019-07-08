be992a7dc6bfb6a1f5169dcf0b3cc14ff13ae9e2
Fei Long Wang <flwang@catalyst.net.nz>
2019-07-08 15:45:21 +1200

    [fedora-atomic][k8s] Support operating system upgrade
    Along with the kubernetes version upgrade support we just released, we're
    adding the support to upgrade the operating system of the k8s cluster
    (including master and worker nodes). It's an inplace upgrade leveraging the
    atomic/ostree upgrade capability.
    
    Story: 2002210
    Task: 33607
    
    Change-Id: If6b9c054bbf5395c30e2803314e5695a531c22bc
    (cherry picked from commit 09f85f37460e8de2002122e2ede469b0f55ce193)

--- magnum/drivers/common/templates/kubernetes/fragments/upgrade-kubernetes.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/upgrade-kubernetes.sh
@@ -6,8 +6,11 @@ set -x
 ssh_cmd="ssh -F /srv/magnum/.ssh/config root@localhost"
 KUBECONFIG="/etc/kubernetes/kubelet-config.yaml"
 new_kube_tag="$kube_tag_input"
+new_ostree_remote="$ostree_remote_input"
+new_ostree_commit="$ostree_commit_input"
+HOSTNAME_OVERRIDE="$(cat /etc/hostname | head -1 | sed 's/\.novalocal//')"
 
-if [ ${new_kube_tag}!=${KUBE_TAG} ]; then
+function drain {
     # If there is only one master and this is the master node, skip the drain, just cordon it
     # If there is only one worker and this is the worker node, skip the drain, just cordon it
     all_masters=$(kubectl get nodes --selector=node-role.kubernetes.io/master= -o name)
@@ -17,6 +20,11 @@ if [ ${new_kube_tag}!=${KUBE_TAG} ]; then
     else
         kubectl cordon ${INSTANCE_NAME}
     fi
+}
+
+if [ "${new_kube_tag}" != "${KUBE_TAG}" ]; then
+
+    drain
 
     SERVICE_LIST=$($ssh_cmd podman ps -f name=kube --format {{.Names}})
 
@@ -35,9 +43,61 @@ if [ ${new_kube_tag}!=${KUBE_TAG} ]; then
     i=0
     until kubectl uncordon ${INSTANCE_NAME}
     do
-        ((i++))
+        i=$((i+1))
         [ $i -lt 30 ] || break;
         echo "Trying to uncordon node..."
         sleep 5s
     done
+fi
+
+function setup_uncordon {
+    # Create a service to uncordon the node itself after reboot
+    if [ ! -f /etc/systemd/system/uncordon.service ]; then
+        $ssh_cmd cat > /etc/systemd/system/uncordon.service << EOF
+[Unit]
+Description=magnum-uncordon
+After=network.target kubelet.service
+
+[Service]
+Restart=Always
+RemainAfterExit=yes
+ExecStart=${kubecontrol} uncordon ${HOSTNAME_OVERRIDE}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+        ${ssh_cmd} systemctl enable uncordon.service
+    fi
+}
+
+remote_list=`${ssh_cmd} ostree remote list`
+# Fedora Atomic 29 will be the last release before migrating to Fedora CoreOS, so we're OK to add 28 and 29 remotes directly
+if [[ ! " ${remote_list[@]} " =~ "fedora-atomic-28" ]]; then
+    ${ssh_cmd} ostree remote add --set=gpgkeypath=/etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-28-primary --contenturl=mirrorlist=https://ostree.fedoraproject.org/mirrorlist fedora-atomic-28 https://kojipkgs.fedoraproject.org/atomic/repo/
+fi
+if [[ ! " ${remote_list[@]} " =~ "fedora-atomic-29" ]]; then
+    ${ssh_cmd} ostree remote add --set=gpgkeypath=/etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-29-primary --contenturl=mirrorlist=https://ostree.fedoraproject.org/mirrorlist fedora-atomic-29 https://kojipkgs.fedoraproject.org/atomic/repo/
+fi
+# The uri of existing Fedora Atomic 27 remote is not accessible now, so replace it with correct uri
+if [[ " ${remote_list[@]} " =~ "fedora-atomic" ]]; then
+    sed -i '
+        /^url=/ s|=.*|=https://kojipkgs.fedoraproject.org/atomic/repo/|
+    ' /etc/ostree/remotes.d/fedora-atomic.conf
+fi
+
+current_ostree_commit=`${ssh_cmd} rpm-ostree status | grep Commit | awk '{print $2}'`
+current_ostree_remote=`${ssh_cmd} rpm-ostree status | awk '/* ostree/{print $0}' | awk '{match($0,"* ostree://([^ ]+)",a)}END{print a[1]}'`
+
+# NOTE(flwang): 1. Either deploy or rebase for only one upgrade
+#               2. Using rpm-ostree command instead of atomic command to keep the possibility of supporting fedora coreos 30
+if [ "$new_ostree_commit" != "" ] && [ "$current_ostree_commit" != "$new_ostree_commit" ]; then
+    drain
+    setup_uncordon
+    ${ssh_cmd} rpm-ostree deploy $new_ostree_commit
+    shutdown --reboot --no-wall -t 1
+elif [ "$new_ostree_remote" != "" ] && [ "$current_ostree_remote" != "$new_ostree_remote" ]; then
+    drain
+    setup_uncordon
+    ${ssh_cmd} rpm-ostree rebase $new_ostree_remote
+    shutdown --reboot --no-wall -t 1
 fi
