3674b3617a770bd71d09e23137ff96f90eb1241a
Spyros Trigazis <spyridon.trigazi@cern.ch>
2019-09-30 15:47:31 +0000

    k8s_atomic: Run all syscontainer with podman
    Using the atomic cli to install kubelet breaks mount
    propagation of secrets, configmaps and so on. Using podman
    in a systemd unit works.
    
    Additionally, with this change all atomic commands are dropped,
    containers are pulled from gcr.io (ofiicial kubernetes containers).
    
    Finally, after this patch only by starting the heat-agent with
    ignition, we can use fedora coreos as a drop-in replacement.
    
    * Drop del of docker0
      This command to remove docker0 is carried from
      earlier versions of docker. This is not an issue
      anymore.
    
    story: 2006459
    task: 36871
    
    Change-Id: I2ed8e02f5295e48d371ac9e1aff2ad5d30d0c2bd
    Signed-off-by: Spyros Trigazis <spyridon.trigazi@cern.ch>

--- magnum/drivers/common/templates/kubernetes/fragments/enable-services-minion.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/enable-services-minion.sh
@@ -9,7 +9,6 @@ ssh_cmd="ssh -F /srv/magnum/.ssh/config root@localhost"
 # be re-created using the flannel-provided subnet).
 echo "stopping docker"
 $ssh_cmd systemctl stop docker
-$ssh_cmd ip link del docker0
 
 # make sure we pick up any modified unit files
 $ssh_cmd systemctl daemon-reload
@@ -17,5 +16,5 @@ $ssh_cmd systemctl daemon-reload
 for service in docker kubelet kube-proxy; do
     echo "activating service $service"
     $ssh_cmd systemctl enable $service
-    $ssh_cmd systemctl --no-block start $service
+    $ssh_cmd systemctl start $service
 done
