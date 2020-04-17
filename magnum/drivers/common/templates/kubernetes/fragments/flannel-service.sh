9c61de289845ef62646d4959a04fd13836063e02
Xinliang Liu <xinliang.liu@linaro.org>
2020-04-17 02:52:15 +0000

    Remove nodeSelector for flannel DaemonSet
    According to upstream kube-flannel.yml PR[1], node-selector introduced
    is because flannel image doesn't support multiarch manifestes. Which
    means that it can't specify flannel:version-arch images in the same
    daemonset for every arch platform. To make every arch platform can
    deploy flannel upstream add one daemonset with nodeSelector per arch.
    
    But in magnum flannel image tag is configurable via label, thus every
    arch platform can use one daemonset to deploy by specify corresponding
    flannel image tag. So nodeSelector is unnecessary here.
    
    [1]: https://github.com/coreos/flannel/pull/989
    
    Change-Id: I97e78e8d77973e03eeff598b212287945ca00190
    Task: 39453
    Story: 2007026
    (cherry picked from commit 6a3762173279dd7fd85d767eb7d6e07b4760270c)

--- magnum/drivers/common/templates/kubernetes/fragments/flannel-service.sh
+++ magnum/drivers/common/templates/kubernetes/fragments/flannel-service.sh
@@ -159,7 +159,7 @@ data:
 apiVersion: apps/v1
 kind: DaemonSet
 metadata:
-  name: kube-flannel-ds-amd64
+  name: kube-flannel-ds
   namespace: kube-system
   labels:
     tier: node
@@ -181,8 +181,6 @@ spec:
         seLinuxOptions:
           type: "spc_t"
       hostNetwork: true
-      nodeSelector:
-        beta.kubernetes.io/arch: amd64
       tolerations:
       - operator: Exists
         effect: NoSchedule
