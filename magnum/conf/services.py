a1fa999c692429ce2fc81447370038e6ad9c6958
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-08-24 15:39:18 +0900

    Drop description for ZeroMQ
    ZeroMQ support by oslo.messaging was removed during Stein cycle so
    the description is no longer useful.
    
    Change-Id: If5163eeaa83b130623da0246880661efff5a2782
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- magnum/conf/services.py
+++ magnum/conf/services.py
@@ -19,9 +19,7 @@ service_opts = [
                        help=_('Name of this node. This can be an opaque '
                               'identifier. It is not necessarily a hostname, '
                               'FQDN, or IP address. However, the node name '
-                              'must be valid within an AMQP key, and if using '
-                              'ZeroMQ, a valid hostname, FQDN, or IP '
-                              'address.')),
+                              'must be valid within an AMQP key.')),
 ]
 
 
