600ac9bf1b4059e0de1d92dc9d2cd3ea68712058
Michal Nasiadka <mnasiadka@gmail.com>
2026-03-02 15:04:56 +0100

    Remove dockerfiles that are not used anymore
    Change-Id: I34200040b844702f6d22051cab96bff32f34f17d
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>

--- dockerfiles/kubernetes-scheduler/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. /etc/kubernetes/scheduler
-. /etc/kubernetes/config
-
-ARGS="$@ $KUBE_LOGTOSTDERR $KUBE_LOG_LEVEL $KUBE_MASTER $KUBE_SCHEDULER_ARGS"
-
-exec /usr/local/bin/kube-scheduler $ARGS
