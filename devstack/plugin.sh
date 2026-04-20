083de3622344bd011eb8ac50663b425a1fefd6fd
Michal Nasiadka <mnasiadka@gmail.com>
2026-04-20 19:50:26 +0200

    devstack: Add function to copy kubeconfig from d-p-c k8s-capi
    Change-Id: I8f85a9f5ed60040f9c89b8750d267ee75e656fe5
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>
    (cherry picked from commit f7ddcfd967e26a66d3df8592b695fd91beaf9301)
    (cherry picked from commit 9089fdf01346a1b07a23d8186f9f081e161d224b)

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -14,6 +14,7 @@ if is_service_enabled magnum-api magnum-cond; then
         install_magnum
 
         MAGNUM_GUEST_IMAGE_URL=${MAGNUM_GUEST_IMAGE_URL:-"https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/35.20220116.3.0/x86_64/fedora-coreos-35.20220116.3.0-openstack.x86_64.qcow2.xz"}
+        MAGNUM_GUEST_IMAGE_EXTRA_PROPERTIES=${MAGNUM_GUEST_IMAGE_EXTRA_PROPERTIES:-""}
         IMAGE_URLS+=",${MAGNUM_GUEST_IMAGE_URL}"
 
         LIBS_FROM_GIT="${LIBS_FROM_GIT},python-magnumclient"
@@ -23,6 +24,10 @@ if is_service_enabled magnum-api magnum-cond; then
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         echo_summary "Configuring magnum"
         configure_magnum
+
+        if is_service_enabled k8s-capi; then
+            copy_kubeconfig
+        fi
 
         # Hack a large timeout for now
         iniset /etc/keystone/keystone.conf token expiration 7200
