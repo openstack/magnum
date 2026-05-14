78d090e65f2bc91729ce06f7e753ed77c763fbf5
Michal Nasiadka <mnasiadka@gmail.com>
2026-05-14 09:58:56 +0200

    Drop keystonemiddleware legacy auth options
    keystonemiddleware dropped admin_user, admin_password, admin_tenant_name,
    auth_uri and related v2.0-era options from [keystone_authtoken] in the
    2026.2 release (removal of AuthTokenPlugin).
    
    Remove the _get_legacy_auth() fallback and its try/except in _get_auth(),
    the import_group() call that pulled those options into Magnum's config,
    and the legacy_session_opts deprecated aliases for TLS session options.
    Update auth_url to read from [keystone_auth].auth_url first, falling back
    to [keystone_authtoken].www_authenticate_uri. Remove the auth_uri fallback
    and v2.0->v3 URL rewrite from hooks.py and keystone.py.
    
    Change-Id: I04aafe25068ea82e142cf97da4936d291ef00150
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>

--- magnum/conf/keystone.py
+++ magnum/conf/keystone.py
@@ -16,25 +16,13 @@ from oslo_config import cfg
 CFG_GROUP = 'keystone_auth'
 CFG_LEGACY_GROUP = 'keystone_authtoken'
 
-legacy_session_opts = {
-    'certfile': [cfg.DeprecatedOpt('certfile', CFG_LEGACY_GROUP)],
-    'keyfile': [cfg.DeprecatedOpt('keyfile', CFG_LEGACY_GROUP)],
-    'cafile': [cfg.DeprecatedOpt('cafile', CFG_LEGACY_GROUP)],
-    'insecure': [cfg.DeprecatedOpt('insecure', CFG_LEGACY_GROUP)],
-    'timeout': [cfg.DeprecatedOpt('timeout', CFG_LEGACY_GROUP)],
-}
-
 keystone_auth_group = cfg.OptGroup(name=CFG_GROUP,
                                    title='Options for Keystone in Magnum')
 
 
 def register_opts(conf):
-    # FIXME(pauloewerton): remove import of authtoken group and legacy options
-    # after deprecation period
-    conf.import_group(CFG_LEGACY_GROUP, 'keystonemiddleware.auth_token')
     ka_loading.register_auth_conf_options(conf, CFG_GROUP)
-    ka_loading.register_session_conf_options(
-        conf, CFG_GROUP, deprecated_opts=legacy_session_opts)
+    ka_loading.register_session_conf_options(conf, CFG_GROUP)
     conf.set_default('auth_type', default='password', group=CFG_GROUP)
 
 
