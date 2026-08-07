1a6a0b2350d26fbe45f80822ce462c339ed9c663
Michal Nasiadka <mnasiadka@gmail.com>
2026-08-07 20:22:07 +0200

    Raise clear error when image os_distro is unset
    Change-Id: Ib3b6054d59e1df2531fbbff57fcf640e1ac2deea
    Signed-off-by: Michal Nasiadka <mnasiadka@gmail.com>
    (cherry picked from commit 23a43e879d5ea72e80f211212567450fe60805d5)
    (cherry picked from commit c25bc6fd5296b982b92b647fd832ca0c94def9b7)

--- magnum/tests/unit/api/test_validation.py
+++ magnum/tests/unit/api/test_validation.py
@@ -384,3 +384,19 @@ class TestValidation(base.BaseTestCase):
             else:
                 self.assertRaises(exception.InvalidParameterValue,
                                   v.validate_cluster_properties, set([field]))
+
+    @mock.patch('pecan.request')
+    @mock.patch('magnum.common.clients.OpenStackClients')
+    def test_enforce_driver_supported_image_os_distro_unset(
+            self, mock_os_clients, mock_pecan_request):
+        @v.enforce_driver_supported()
+        def test(self, cluster_template):
+            pass
+        mock_os_clients.return_value.glance.return_value.find_image\
+            .return_value = mock.MagicMock(os_distro=None, properties={})
+        cluster_template = mock.MagicMock()
+        cluster_template.cluster_distro = None
+        cluster_template.driver = None
+        cluster_template.image_id = 'test-image-id'
+        self.assertRaises(exception.OSDistroFieldNotFound,
+                          test, self, cluster_template)
